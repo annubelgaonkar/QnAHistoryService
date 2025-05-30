a042fef046aa59287d88df4a8f5f997996460a5c
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2025-05-30 12:06:43 +0530

    HistoryService for learning app

---
+++ src/main/java/dev/qna/qna_history_service/QnAHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.qna.qna_history_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnAHistoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QnAHistoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/qna/qna_history_service/QnAHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.qna.qna_history_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnAHistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
